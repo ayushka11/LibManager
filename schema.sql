97f9615a5db8b0b01f0715881008f19d875e1e48
ayushka11 <gargayushka@gmail.com>
2024-06-05 17:31:57 +0530

    updated schema.sql
    Signed-off-by: ayushka11 <gargayushka@gmail.com>

--- schema.sql
+++ schema.sql
@@ -1,1 +1,32 @@
-CREATE DATABASE library;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    isAdmin BOOLEAN DEFAULT false
+);
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE checkouts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    checkout_date DATE,
+    due_date DATE,
+    return_date DATE,
+    fine DECIMAL(5,2) DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+CREATE TABLE admin_requests (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    status VARCHAR(20) DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
